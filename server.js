675bb54a8eb0bca22cc9820ee0d61f7e64d5f23d
ad3rinto <aad3rinto@gmail.com>
2023-04-09 18:54:46 +0100

    added new route

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require("express")
 const app = express();
 const PORT = 3000;
 
+app.get("/", function (req, res) {
+    console.log(req)
+    res.send("<h1>Home</h1>")
+})
+
+app.get("/contact", function (req, res) {
+    console.log(req)
+    res.send("<h1>Contact</h1>")
+})
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
